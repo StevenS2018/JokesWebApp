8c6254ec1f63df64382bc870c61bfcd9d13310c5
ssand <ssand@MSI>
2023-04-29 16:21:34 -0500

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string jokeQuestion { get; set; }
+
+        public string jokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+
+    }
+}
